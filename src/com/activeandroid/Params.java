680c0c71fc37ffa635514adc457fcb9fa1e77565
Michael Pardo <pardom@mobiata.com>
2010-10-25 08:54:40 -0400

    Add version to params.

--- src/com/activeandroid/Params.java
+++ src/com/activeandroid/Params.java
@@ -1,6 +1,7 @@
 package com.activeandroid;
 
 class Params {
+	public static final String VERSION = "2.0.0";
 	public static final boolean IS_TRIAL = false;
 	
 	public static final boolean LOGGING_ENABLED = true;
