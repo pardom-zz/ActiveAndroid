4ad8e666423ebbf1eec68dddf76b3e809991dee6
Alex May <alex.k.may@gmail.com>
2013-01-24 19:37:51 -0800

    allow model to be instantiated if initialize has not been called

--- src/com/activeandroid/Cache.java
+++ src/com/activeandroid/Cache.java
@@ -108,6 +108,9 @@ public final class Cache {
 	// Entity cache
 
 	public static synchronized void addEntity(Model entity) {
+		if (sEntities == null) {
+			return;
+		}
 		sEntities.add(entity);
 	}
 
@@ -134,6 +137,9 @@ public final class Cache {
 	}
 
 	public static synchronized TableInfo getTableInfo(Class<? extends Model> type) {
+		if (sModelInfo == null) {
+			return null;
+		}
 		return sModelInfo.getTableInfo(type);
 	}
 
