39a27bf50a9fef48dcb9e2d0d6e524c8d3ffb16f
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2013-11-23 08:25:10 +0900

    Add Cache test

---
+++ tests/src/com/activeandroid/test/CacheTest.java
@@ -0,0 +1,49 @@
+package com.activeandroid.test;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Cache;
+import com.activeandroid.Configuration;
+import com.activeandroid.Model;
+import com.activeandroid.TableInfo;
+import com.activeandroid.annotation.Table;
+
+import android.test.AndroidTestCase;
+
+import java.util.Collection;
+
+public class CacheTest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() {
+        Configuration conf = new Configuration.Builder(getContext())
+                .addModelClasses(CacheTestModel.class, CacheTestModel2.class)
+                .create();
+        ActiveAndroid.initialize(conf, true);
+    }
+
+    public void testGetTableInfos() {
+        assertNotNull(Cache.getContext());
+        Collection<TableInfo> tableInfos = Cache.getTableInfos();
+        assertEquals(2, tableInfos.size());
+
+        {
+            TableInfo tableInfo = Cache.getTableInfo(CacheTestModel.class);
+            assertNotNull(tableInfo);
+            assertEquals("CacheTestModel", tableInfo.getTableName());
+        }
+
+        {
+            TableInfo tableInfo = Cache.getTableInfo(CacheTestModel2.class);
+            assertNotNull(tableInfo);
+            assertEquals("CacheTestModel2", tableInfo.getTableName());
+        }
+    }
+
+    @Table(name = "CacheTestModel")
+    private static class CacheTestModel extends Model {
+    }
+
+    @Table(name = "CacheTestModel2")
+    private static class CacheTestModel2 extends Model {
+    }
+}
