f2fb1b9af277db39f8c3de59441a30a4fce288d7
zzss <zsolt.kocsi@gmail.com>
2015-03-03 00:24:25 +0100

    Catch IncompatibleClassChangeError when scanning for model classes

--- src/com/activeandroid/ModelInfo.java
+++ src/com/activeandroid/ModelInfo.java
@@ -204,6 +204,9 @@ final class ModelInfo {
 			catch (IllegalAccessException e) {
 				Log.e("IllegalAccessException", e);
 			}
+			catch (IncompatibleClassChangeError e) {
+				Log.e("IncompatibleClassChangeError", e);
+			}
 		}
 	}
 }
